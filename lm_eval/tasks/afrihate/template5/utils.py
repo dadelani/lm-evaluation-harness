4d52009a369137ce1a83aaa1052bf2c211d65296
dadelani <davlanade@gmail.com>
2024-08-31 21:36:57 -0400

    add update

---
+++ lm_eval/tasks/afrihate/template5/utils.py
@@ -0,0 +1,27 @@
+from lm_eval.utils import weighted_f1_score
+
+
+def doc_to_text(doc):
+    output = """I am providing you with the definition Hate speech, Abusive language and Normal tweets.
+    
+    An Hate speech is language content that expresses hatred towards a particular group or individual based on their 
+    political affiliation, race, ethnicity, religion, gender, sexual orientation, or other characteristics. 
+    It also includes threats of violence.
+
+    Abusive language is any form of bad language expressions including rude, impolite, insulting or 
+    belittling utterance intended to offend or harm an individual. 
+    
+    Normal does not contain any bad language. 
+
+    Tweet: {tweet}
+    
+    Which category does the tweet above belong to: 'Hate', 'Abuse' or 'Normal'. Pick exactly one category.  
+    """
+
+    text = output.format(premise=doc["tweet"])
+    return text
+
+
+def doc_to_target(doc):
+    replacements = {0: "Hate", 1: "Abuse", 2: "Normal"}
+    return replacements[doc["label"]]
--- lm_eval/utils.py
+++ lm_eval/utils.py
@@ -499,3 +499,13 @@ def weighted_f1_score(items):
     preds = unzipped_list[1]
     fscore = f1_score(golds, preds, average="weighted")
     return fscore
+
+def macro_f1_score(items):
+    from sklearn.metrics import f1_score
+
+    unzipped_list = list(zip(*items))
+    golds = unzipped_list[0]
+    preds = unzipped_list[1]
+    fscore = f1_score(golds, preds, average="macro")
+    return fscore
+
